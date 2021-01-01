d43d977e8b7b4ef090bd8c298f6165ae8859efe9
William J. Bowman <wjb@williamjbowman.com>
2021-01-01 00:14:18 -0800

    Tweaks to milestone2 skeleton
    - Add a few hints about structure of tricky passes
    - Fix provide interface

--- main.rkt
+++ main.rkt
@@ -5,6 +5,7 @@
  cpsc411/2c-run-time)
 
 (provide
+ check-values-lang
  uniquify
  sequentialize-let
  canonicalize-bind
@@ -17,10 +18,74 @@
  patch-instructions
  implement-fvars
  check-paren-x64
- generate-x64)
+ generate-x64
+
+ interp-values-lang
+
+ interp-paren-x64)
 
 ;; TODO: Fill in.
 ;; You might want to reuse check-paren-x64 and generate-x64 from milestone-1
+
+(define (select-instructions p)
+
+  ; (Imp-cmf-lang-v3 value) -> (List-of (Asm-lang-v2 effect)) and (Asm-lang-v2 aloc)
+  ; Assigns the value v to a fresh temporary, returning two values: the list of
+  ; statements the implement the assignment in Loc-lang, and the aloc that the
+  ; value is stored in.
+  (define (assign-tmp v)
+    (TODO "Consider implementing assign-tmp."))
+
+  (define (select-tail e)
+    (TODO "Implement select-tail"))
+
+  (define (select-value e)
+    (TODO "Implement select-value"))
+
+  (define (select-effect e)
+    (TODO "Implement select-value"))
+
+  (match p
+    [`(module ,tail)
+     `(module () ,(select-tail tail))]))
+
+(define (interp-paren-x64 p)
+  ; Environment (List-of (paren-x64-v2 Statements)) -> Integer
+  (define (eval-instruction-sequence env sls)
+    (if (null? sls)
+        (dict-ref env 'rax)
+        (TODO "Implement the fold over a sequence of Paren-x64-v2 /s/.")))
+
+  ; Environment Statement -> Environment
+  (define (eval-statement env s)
+    (TODO "Implement the transition function evaluating a Paren-x64-v2 /s/."))
+
+  ; (Paren-x64-v2 binop) -> procedure?
+  (define (eval-binop b)
+    (TODO "Implement the interpreter for Paren-x64-v2 /binop/."))
+
+  ; Environment (Paren-x64-v2 triv) -> Integer
+  (define (eval-triv regfile t)
+    (TODO "Implement the interpreter for Paren-x64-v2 /triv/."))
+
+  (TODO "Implement the interpreter for Paren-x64-v2 /p/."))
+
+(define (generate-x64 p)
+  (define (program->x64 p)
+    (match p
+      [`(begin ,s ...)
+       (TODO ...)]))
+
+  (define (statement->x64 s)
+    (TODO ...))
+
+  (define (loc->x64 loc)
+    (TODO ...))
+
+  (define (binop->ins b)
+    (TODO ...))
+
+  (program->x64 p))
 
 (module+ test
   (require
@@ -31,6 +96,7 @@
   (run-tests
    (a2-public-test-suite
     (list
+     check-values-lang
      uniquify
      sequentialize-let
      canonicalize-bind
@@ -43,6 +109,6 @@
      patch-instructions
      implement-fvars
      check-paren-x64
-     generate-x64
-     wrap-x64-run-time
-     wrap-x64-boilerplate))))
+     generate-x64)
+    interp-values-lang
+    interp-paren-x64)))
