bcdaa1a5452a9ec425a5d4aa64d89c35987a025d
William J. Bowman <wjb@williamjbowman.com>
2020-12-31 21:13:01 -0800

    Added milestone 1 skeleton

---
+++ interface-test.rkt
@@ -0,0 +1,13 @@
+#lang racket/base
+
+(module+ test
+  (require
+   cpsc411/test-suite/utils)
+
+  (check-import-list
+   "main.rkt"
+   '(check-paren-x64
+     interp-paren-x64
+     generate-x64
+     wrap-x64-run-time
+     wrap-x64-boilerplate)))
