bbf97801a2b6726f426cf8a79052c2ed148c4fdf
William J. Bowman <wjb@williamjbowman.com>
2021-02-12 20:25:45 -0800

    Add some extra skeleton code
    - Skeleton of interp-paren-x64
    - An example of testing infinite loops

--- compiler.rkt
+++ compiler.rkt
@@ -30,12 +30,37 @@
 ;; TODO: Fill in.
 ;; You'll want to merge milestone-3 code in
 
+(define (link-paren-x64 p)
+  (TODO "Design and implement link-paren-x64 for Exercise 2."))
+
+;; Exercise 3
+(define (interp-paren-x64 p)
+
+  (define (eval-program env pc p)
+    (TODO "Finish designing eval-program for Exercise 3")
+    (if (= pc (length p))
+        (dict-ref env 'rax)
+        (eval-statement env pc p (list-ref p pc))))
+
+  (TODO "Redesign and implement interp-paren-x64 for Exercise 3."))
+
 (module+ test
   (require
    rackunit
    rackunit/text-ui
    cpsc411/langs/a4
    cpsc411/test-suite/public/a4)
+
+  (define-check (check-timeout? th seconds)
+    (when (sync/timeout seconds (thread th))
+      (fail-check)))
+
+  (check-timeout?
+   (thunk
+    (interp-paren-x64
+     '(begin
+        (define L.f.10 (jump L.f.10)))))
+   2)
 
   ;; You can modify this pass list, e.g., by adding check-assignment, or other
   ;; debugging and validation passes.
