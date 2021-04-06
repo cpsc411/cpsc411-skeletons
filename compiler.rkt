636811b73cc63900587dc096fa433c48221b7aa5
William J. Bowman <wjb@williamjbowman.com>
2021-04-05 19:37:16 -0700

    Add a9 skeleton

--- compiler.rkt
+++ compiler.rkt
@@ -8,6 +8,15 @@
  check-exprs-lang
  uniquify
  implement-safe-primops
+ implement-safe-call
+ define->letrec
+ optimize-direct-calls
+ dox-lambdas
+ uncover-free
+ convert-closures
+ optimize-known-calls
+ hoist-lambdas
+ implement-closures
  specify-representation
  remove-complex-opera*
  sequentialize-let
@@ -33,14 +42,14 @@
  generate-x64)
 
 ;; TODO: Fill in.
-;; You'll want to merge milestone-7 code in
+;; You'll want to merge milestone-8 code in
 
 (module+ test
   (require
    rackunit
    rackunit/text-ui
    cpsc411/test-suite/utils
-   cpsc411/test-suite/public/a8
+   cpsc411/test-suite/public/a9
    #;errortrace)
 
   ;; You can modify this pass list, e.g., by adding other
@@ -55,6 +64,15 @@
     check-exprs-lang
     uniquify
     implement-safe-primops
+    implement-safe-call
+    define->letrec
+    optimize-direct-calls
+    dox-lambdas
+    uncover-free
+    convert-closures
+    optimize-known-calls
+    hoist-lambdas
+    implement-closures
     specify-representation
     remove-complex-opera*
     sequentialize-let
@@ -84,12 +102,21 @@
   ;; Toggle to #f to enable fragile tests
   (parameterize ([current-enable-grading #t])
     (run-tests
-     (a8-public-test-suite
+     (a9-public-test-suite
       (current-pass-list)
 
       check-exprs-lang
       uniquify
       implement-safe-primops
+      implement-safe-call
+      define->letrec
+      optimize-direct-calls
+      dox-lambdas
+      uncover-free
+      convert-closures
+      optimize-known-calls
+      hoist-lambdas
+      implement-closures
       specify-representation
       remove-complex-opera*
       sequentialize-let
